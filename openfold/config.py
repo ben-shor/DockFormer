c01df6bffd213fb05a084fc38d51d40056cfe1a8
Ben Shor <benshor22@gmail.com>
2024-06-12 12:53:04 +0300

    minor fixes

--- openfold/config.py
+++ openfold/config.py
@@ -310,7 +310,7 @@ config = mlc.ConfigDict(
                 "weight": 0.0,
             },
             "binding_site": {
-                "weight": 0.05,
+                "weight": 0.0,
                 "pos_class_weight": 20.0,
             },
             "affinity2d": {
